a816f6987873bb4db52ea162a0e80f79d22c17ab
Julio Muller <juliodolago@gmail.com>
2019-12-13 00:18:39 -0300

    Defined DAO for 'Customer'

--- src/br/ufpr/lpoo2/model/dao/CustomerDAO.java
+++ src/br/ufpr/lpoo2/model/dao/CustomerDAO.java
@@ -2,101 +2,108 @@ package br.ufpr.lpoo2.model.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Date;
 import java.sql.ResultSet;
-import java.sql.Statement;
-import java.time.LocalDate;
-import java.util.ArrayList;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.ArrayList;
 import br.ufpr.lpoo2.model.Customer;
-import br.ufpr.lpoo2.services.ConnectionFactory;
+import br.ufpr.lpoo2.services.ConnectionFactory;;
 
 public class CustomerDAO {
 
-    private final ConnectionFactory connectionFactory;
-    private final String insert = "INSERT INTO customers(first_name, last_name, date_birth, address, rg, cpf) VALUES (?, ?, ?, ?, ?, ?)";
-    private final String select = "SELECT id, first_name, last_name, date_birth, address, rg, cpf FROM customers";
-    private final String update = "UPDATE customers SET first_name = ?, last_name = ?, date_birth = ?, address = ?, rg = ?, cpf = ? WHERE id = ?";
-    private final String delete = "DELETE FROM customers WHERE id = ?";
+    private final ConnectionFactory connection;
+    private final String INSERT_CUSTOMER = "INSERT INTO customers (first_name, last_name, rg, cpf, address) VALUES (?, ?, ?, ?, ?)";
+    private final String SELECT_ALL_CUSTOMERS = "SELECT id, first_name, last_name, rg, cpf, address FROM customers";
+    private final String SELECT_CUSTOMER_BY_CPF = "SELECT first_name, last_name, rg, address FROM customers WHERE cpf = ?";
+    private final String UPDATE_CUSTOMER = "UPDATE customers SET first_name = ?, last_name = ?, rg = ?, cpf = ?, address = ? WHERE id = ?";
+    private final String DELETE_CUSTOMER = "DELETE FROM customers WHERE id = ?";
 
-    public CustomerDAO(ConnectionFactory conFactory) {
-        this.connectionFactory = conFactory;
-        connectionFactory.getConnection();
+    public CustomerDAO(ConnectionFactory conn) throws SQLException {
+        this.connection = conn;
+        connection.getConnection();
     }
 
-    public void insert(Customer customer) {
-        Connection conn = connectionFactory.getConnection();
-        try {
-            PreparedStatement stmt = conn.prepareStatement(this.insert, Statement.RETURN_GENERATED_KEYS);
-            stmt.setString(1, customer.getFirstName());
-            stmt.setString(2, customer.getLastName());
-            stmt.setDate(3, Date.valueOf(customer.getDateBirth()));
-            stmt.setString(4, customer.getAddress());
-            stmt.setString(5, customer.getRG());
-            stmt.setString(6, customer.getCPF());
-            stmt.execute();
-            ResultSet rs = stmt.getGeneratedKeys();
-            rs.next();
-            long i = rs.getLong(1);
-            customer.setId(i);
-        } catch (SQLException ex) {
-            throw new RuntimeException(ex);
-        }
-    }
-
-    public List<Customer> selectAll() throws SQLException {
-        Connection conn = connectionFactory.getConnection();
-        ResultSet rs = null;
-        PreparedStatement stmt = conn.prepareStatement(this.select);
-        try {
-            rs = stmt.executeQuery();
+    public List<Customer> list() throws SQLException {
+        try (Connection conn = connection.getConnection()) {
+            PreparedStatement stmt = conn.prepareStatement(SELECT_ALL_CUSTOMERS);
+            ResultSet rs = stmt.executeQuery();
             List<Customer> customers = new ArrayList<>();
             while (rs.next()) {
                 customers.add(new Customer(
-                    rs.getLong("id"),
+                    rs.getInt("id"),
                     rs.getString("first_name"),
                     rs.getString("last_name"),
-                    rs.getDate("date_birth").toLocalDate(),
-                    rs.getString("address"),
                     rs.getString("rg"),
-                    rs.getString("cpf")
+                    rs.getString("cpf"),
+                    rs.getString("address")
                 ));
             }
             return customers;
         } catch (SQLException ex) {
-            throw new RuntimeException(ex);
-        } finally {
-            rs.close();
-            stmt.close();
+            throw new RuntimeException("Erro ao consultar uma lista de clientes. Origem: " + ex.getMessage());
+        }
+    }
+
+    public Customer get(String cpf) throws SQLException {
+        try (Connection conn = connection.getConnection()) {
+            PreparedStatement stmt = conn.prepareStatement(SELECT_CUSTOMER_BY_CPF);
+            stmt.setString(1, cpf);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return new Customer(
+                    null,
+                    rs.getString("first_name"),
+                    rs.getString("last_name"),
+                    rs.getString("rg"),
+                    cpf,
+                    rs.getString("address")
+                );
+            }
+            return null;
+        } catch (SQLException ex) {
+            throw new RuntimeException("Erro ao consultar um cliente. Origem=" + ex.getMessage());
+        }
+    }
+
+    public void insert(Customer customer) throws SQLException {
+        try (Connection conn = connection.getConnection()) {
+            PreparedStatement stmt = conn.prepareStatement(INSERT_CUSTOMER, PreparedStatement.RETURN_GENERATED_KEYS);
+            stmt.setString(1, customer.getFirstName());
+            stmt.setString(2, customer.getLastName());
+            stmt.setString(3, customer.getRG());
+            stmt.setString(4, customer.getCPF());
+            stmt.setString(5, customer.getAddress());
+            stmt.executeUpdate();
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            customer.setId(rs.getInt(1));
+        } catch (SQLException ex) {
+            throw new RuntimeException("Erro ao inserir um cliente no banco de dados. Origem: " + ex.getMessage());
         }
     }
 
     public void update(Customer customer) throws SQLException {
-        Connection conn = connectionFactory.getConnection();
-        PreparedStatement stmt = conn.prepareStatement(this.update);
-        try {
+        try (Connection conn = connection.getConnection()) {
+            PreparedStatement stmt = conn.prepareStatement(UPDATE_CUSTOMER);
             stmt.setString(1, customer.getFirstName());
             stmt.setString(2, customer.getLastName());
-            stmt.setDate(3, Date.valueOf(customer.getDateBirth()));
-            stmt.setString(4, customer.getAddress());
-            stmt.setString(5, customer.getRG());
-            stmt.setString(6, customer.getCPF());
-            stmt.setLong(7, customer.getId());
+            stmt.setString(3, customer.getRG());
+            stmt.setString(4, customer.getCPF());
+            stmt.setString(5, customer.getAddress());
+            stmt.setInt(6, customer.getId());
             stmt.executeUpdate();
-        } finally {
-            stmt.close();
+        } catch (SQLException ex) {
+            throw new RuntimeException("Erro ao atualizar um cliente no banco de dados. Origem: " + ex.getMessage());
         }
     }
 
     public void delete(Customer customer) throws SQLException {
-        Connection conn = connectionFactory.getConnection();
-        PreparedStatement stmt = conn.prepareStatement(this.delete);
-        try {
-            stmt.setLong(1, customer.getId());
+        try (Connection conn = connection.getConnection()) {
+            PreparedStatement stmt = conn.prepareStatement(DELETE_CUSTOMER);
+            stmt.setInt(1, customer.getId());
             stmt.executeUpdate();
-        } finally {
-            stmt.close();
+        } catch (SQLException ex) {
+            throw new RuntimeException("Erro ao excluir um cliente no banco de dados. Origem: " + ex.getMessage());
         }
     }
 }
