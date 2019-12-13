94f54efe53d1b50d1960174e23f0a62fe56ca49a
Julio Muller <juliodolago@gmail.com>
2019-12-13 00:09:56 -0300

    Defined DDL

--- setup.sql
+++ setup.sql
@@ -1,16 +1,23 @@
 
--- PostgreSQL
-
+DROP TABLE IF EXISTS accounts;
 DROP TABLE IF EXISTS customers;
 
-CREATE TABLE customers(
+CREATE TABLE customers (
   id SERIAL PRIMARY KEY,
-  first_name VARCHAR(50) NOT NULL,
-  last_name VARCHAR(150) NOT NULL,
-  date_birth DATE NOT NULL,
-  address VARCHAR,
-  rg CHAR(9),
-  cpf CHAR(11) NOT NULL
+  first_name VARCHAR(50),
+  last_name VARCHAR(100),
+  rg VARCHAR(11),
+  cpf CHAR(11) UNIQUE,
+  address VARCHAR(200)
 );
 
-SELECT * FROM customers;
+CREATE TABLE accounts (
+  id SERIAL PRIMARY KEY,
+  customer_cpf CHAR(11) REFERENCES customers(cpf),
+  tpye VARCHAR(20),
+  balance NUMERIC(12,2),
+  max_limit NUMERIC(12,2),
+  initial_depositl numeric(12,2),
+  min_balance numeric(12,2),
+  min_deposit numeric(12,2)
+);
